88c74f3c8cc3b46d82346f92d6ce8d17010bacb2
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-20 18:20:46 +0530

    Using delete method and dynamic route we can delete items from server

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="max-w-[1300px] min-h-screen">
             <img className="h-[80vh] brightness-50 object-cover object-center w-full" src="https://images.unsplash.com/photo-1459755486867-b55449bb39ff?auto=format&fit=crop&q=80&w=1469&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <div className="absolute flex gap-1 top-1/2 left-96 text-center font-semibold text-3xl text-white italic">
+            <div className="absolute flex gap-1 top-1/2 left-80 text-center font-semibold text-4xl text-white italic">
             <h1>Sip Our Coffee to Make Your Face Happy!</h1> 
             <p><AiFillHeart className="text-white text-4xl"></AiFillHeart></p>
             </div>
--- src/components/CoffeeCard/CoffeeCard.jsx
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -2,9 +2,41 @@ import PropTypes from 'prop-types';
 import {IoEyeSharp} from 'react-icons/io5';
 import {BsFillPencilFill} from 'react-icons/bs';
 import {RxCross1} from 'react-icons/rx';
+import Swal from 'sweetalert2';
 const CoffeeCard = ({coffee}) => {
     // destructure 
     const {_id, name, supplier, category, availableQuantity, taste, details, photo} = coffee;
+    const handleDeleteBtn = (id) =>{
+        console.log(id);
+        // added sweet alert to confirm delete or not 
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                console.log('Delete confirmed');
+                fetch(`http://localhost:5050/coffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if(data.deletedCount > 0){
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+            }
+          })
+    }
     return (
         <div className='bg-[#f9eae0] rounded-xl mb-10'>
             <div className="card card-side shadow-xl">
@@ -16,9 +48,15 @@ const CoffeeCard = ({coffee}) => {
                     <p><span className='font-bold'>Supplier:</span> {supplier}</p>
                 </div>
                 <div className="btn-group btn-group-vertical space-y-2">
-                    <button className="btn bg-blue-600 text-white text-xl hover:bg-blue-800"><IoEyeSharp></IoEyeSharp></button>
-                    <button className="btn bg-black text-xl hover:text-black text-white"><BsFillPencilFill></BsFillPencilFill></button>
-                    <button className="btn text-red-700 text-xl hover:bg-red-700 hover:text-white"><RxCross1></RxCross1></button>
+                    {/* view btn  */}
+                    <button 
+                    className="btn bg-blue-600 text-white text-xl hover:bg-blue-800"><IoEyeSharp></IoEyeSharp></button>
+                    {/* edit btn  */}
+                    <button 
+                    className="btn bg-black text-xl hover:text-black text-white"><BsFillPencilFill></BsFillPencilFill></button>
+                    {/* delete btn with passing id */}
+                    <button onClick={()=>handleDeleteBtn(_id)}
+                    className="btn text-red-700 text-xl hover:bg-red-700 hover:text-white"><RxCross1></RxCross1></button>
                 </div>
             </div>
             </div>
